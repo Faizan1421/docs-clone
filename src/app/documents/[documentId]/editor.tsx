68f699fdff55b1adc32dc597028c3c9e7107afc6
sufiprog <sufidemo@gmail.com>
2024-12-11 08:25:57 +0500

    commit 11/12/24

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -33,22 +33,7 @@ export const Editor = () => {
       ImageResize,
       
     ],
-    content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
-      `,
+    content: 'hello',
   })
 
   return (
