e24d730c0063fc218df22c0e1434e139b27b0c9f
sufiprog <sufidemo@gmail.com>
2024-12-14 17:24:33 +0500

    performing crud

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,19 @@
-'use client' 
+'use client'
 
-import { useQuery } from 'convex/react' 
-import { api } from '../../../convex/_generated/api' 
-import { Navbar } from './navbar' 
-import { TemplatesGallery } from './templates-gallery' 
-import Image from 'next/image' 
-import { FullsceenLoader } from '@/components/fullscreen-loader' 
+import { usePaginatedQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
+import { Navbar } from './navbar'
+import { TemplatesGallery } from './templates-gallery'
+import { FullsceenLoader } from '@/components/fullscreen-loader'
+import { DocumentsTable } from './documents-table'
 
 const Home = () => {
-  const documents = useQuery(api.documents.get)
+  const {
+    results,
+    status,
+    loadMore
 
-  if (documents === undefined) {
-    return (
-      <FullsceenLoader label='Loading...' />
-    )
-  }
+  } = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 })
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -23,9 +22,11 @@ const Home = () => {
       </div>
       <div className='mt-16'>
         <TemplatesGallery />
-        {documents?.map((document) => (
-          <span key={document._id}>{document.title}</span>
-        ))}
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
       </div>
     </div>
   )
